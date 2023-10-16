c0b9916d90f85486f50fbb652ab2b00e6b165481
[GameNetics] <[williamzhang7515@gmail.com]>
2023-10-16 13:19:02 +1100

    new math stuff

--- mathstatisticdata/standarddeviation.py
+++ mathstatisticdata/standarddeviation.py
@@ -1,6 +1,9 @@
 import math
 population = input("Standard deviation calculator v0.0.0.0.0.0.0.1\nData: ").split()
 size = len(population)
+for data in range(0, len(population)):
+  population[data] = int(population[data])
+population.sort()
 mean = 0
 for num in population:
     mean += int(num)
@@ -9,7 +12,6 @@ result = 0
 for num in population:
     result += (int(num)-mean)**2
 #print(result2)
-print("final answer model:sample =", math.sqrt(result/(size-1)), "\n")
+print("Mean: ",mean,"\nfinal answer model:sample =", math.sqrt(result/(size-1)), "\n")
 #print(result)
 print("final answer model:population =", math.sqrt(result/size))
-
